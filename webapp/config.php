7cb392f2efddbf5c319fc48a714d597d729c5b3e
James McKinney <james@slashpoundbang.com>
2011-12-03 11:54:17 -0500

    first commit

---
+++ webapp/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$dbname = "watervoices";
+$user = "root";
+$password = "root";
+
+require dirname(__FILE__) . '/zebra/Zebra_Database.php';
+
+session_start();
+
+ini_set('display_errors', 'On');
+?>
+
